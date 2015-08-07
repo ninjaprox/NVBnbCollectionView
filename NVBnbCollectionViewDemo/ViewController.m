9f833a5d3a458f754f00bbe26bf48006f5c828c2
Nguyen Vinh <ninjaprox@gmail.com>
2015-08-08 00:11:33 +0400

    Test initial custom layout

--- NVBnbCollectionViewDemo/ViewController.m
+++ NVBnbCollectionViewDemo/ViewController.m
@@ -16,12 +16,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 100;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    UILabel *label;
+    
+    if (cell.contentView.subviews.count == 0) {
+        label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 50)];
+        [cell.contentView addSubview:label];
+    } else {
+        label = cell.contentView.subviews[0];
+    }
+    
+    cell.backgroundColor = [UIColor yellowColor];
+    label.text = [[NSString alloc] initWithFormat:@"%d", indexPath.row];
+    NSLog(@"Cell's subviews: %@", cell.contentView.subviews);
+    
+    return cell;
 }
 
 @end
